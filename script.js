dea589fe209a7598af588efded60f404b67c52bc
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-05-22 13:10:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function threeSum(arr, target) {
-// write your code here
-  
+function decimalToBinary(decimal) {
+  // Check if the input is valid
+  if (decimal === 0) {
+    return '0';
+  } else if (decimal < 0 || !Number.isInteger(decimal)) {
+    return 'Invalid input. Please enter a positive integer.';
+  }
+
+  let binary = '';
+  while (decimal > 0) {
+    // Get the remainder when dividing by 2
+    const remainder = decimal % 2;
+    // Append the remainder to the binary string
+    binary = remainder + binary;
+    // Update the decimal by dividing it by 2
+    decimal = Math.floor(decimal / 2);
+  }
+
+  return binary;
 }
+
+console.log(`Binary representation: ${binaryNumber}`);
 
 module.exports = threeSum;
