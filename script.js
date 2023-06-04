7bbbc373634dbdb3683b1e113e883933b910329c
Pratibha-98100 <130212205+Pratibha-98100@users.noreply.github.com>
2023-06-04 15:02:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -22,4 +22,4 @@ console.log(decimalToBinary(7)); // Output: 111
 console.log(decimalToBinary(10)); // Output: 1010
 console.log(decimalToBinary(33)); // Output: 100001
 
-module.exports = decimalToBinary
+module.exports = decimalToBinary;
